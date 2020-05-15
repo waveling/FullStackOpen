609c87de507078147bf9ea8be303ae1837ff10e7
JompAa <joonas.aaltonen@me.com>
2020-05-15 13:52:44 -0700

    exercises 1.1. - 1.2.

---
+++ part1/course_information/src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Part from './Part.js';
+
+const Content = (props) => {
+  return (
+  <div>
+   <Part part={props.part1} exercises={props.exercises1} />
+   <Part part={props.part2} exercises={props.exercises2} />
+   <Part part={props.part3} exercises={props.exercises3} />
+  </div>
+  )
+}
+
+export default Content;
