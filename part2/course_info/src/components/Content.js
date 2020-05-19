a6b7cb5063a3c48f819bc4161dbd4c04419c4f42
JompAa <joonas.aaltonen@me.com>
2020-05-19 16:43:34 -0700

    ex 2.1 - 2.5

--- part2/course_info/src/components/Content.js
+++ part2/course_info/src/components/Content.js
@@ -3,11 +3,9 @@ import Part from './Part'
 
 const Content = ({ course }) => {
   return (
-    <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
-    </div>
+    course.parts.map((item, i) => {
+      return <Part part={item} key={i} />
+    }) 
   )
 }
 
