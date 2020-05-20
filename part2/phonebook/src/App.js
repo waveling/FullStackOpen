cf9872bb7c678d72653cd00f940a4f161bce2bd4
JompAa <joonas.aaltonen@me.com>
2020-05-20 13:51:37 -0700

    ex 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,39 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  /* Input new name in state */
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  /* New object is added to the persons array
+      -Event is the submit event */
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName
+    }
+    console.log(nameObject)
+    setPersons(persons.concat(nameObject));
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
+          {/*  */}
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {/* map over the name-props in objects in persons array */}
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
