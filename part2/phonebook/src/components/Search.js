e70906dd13e4aeb8d1f7cd3813b8bba5632308e6
JompAa <joonas.aaltonen@me.com>
2020-05-21 10:12:34 -0700

    ex 2.7 - 2.8

---
+++ part2/phonebook/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = (props) => {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <h4>Search contact:<input/></h4>
+    </div>
+  )
+}
+
+export default Search;
