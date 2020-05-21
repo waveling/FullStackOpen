b7a17265e2afb5a9955ed7e71f22178a5da4b424
JompAa <joonas.aaltonen@me.com>
2020-05-21 14:29:58 -0700

    removed fluff

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,44 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Search from './components/Search';
+import Form from './components/Form';
+import Display from './components/Display';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [searchParam, setSearchParam] = useState('')
 
   /* Input new name in state */
-  const handleChange = (event) => {
-    console.log(event.target.value)
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  /* New object is added to the persons array
-      -Event is the submit event */
-  const addName = (event) => {
+  const handleNumberChange = (event) => {
+    console.log(newNumber)
+    setNewNumber(event.target.value)
+  }
+  
+  const addContact = (event) => {
     event.preventDefault();
-    const nameObject = {
-      name: newName
+    const contactObject = {
+      name: newName,
+      number: newNumber
     }
-    console.log(nameObject)
-    setPersons(persons.concat(nameObject));
-    setNewName('')
+    persons.some(person => person.name === newName || person.number === newNumber) 
+    ? window.alert(`The contact ${newName} already exists!`)
+    : setPersons(persons.concat(contactObject));
+      setNewName('')
+      setNewNumber('')  
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleChange}/>
-        </div>
-        <div>
-          {/*  */}
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {/* map over the name-props in objects in persons array */}
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      <h1>Phonebook</h1>
+      <Search 
+        searchParam={searchParam} 
+        setSearchParam={setSearchParam} 
+      />
+      <h2>Add new contact</h2>
+      <Form 
+        addContact={addContact} 
+        newName={newName} 
+        handleNameChange={handleNameChange} 
+        newNumber={newNumber} 
+        handleNumberChange={handleNumberChange} 
+      />
+      <h2>Contacts</h2>
+      <Display 
+        persons={persons}
+        searchParam={searchParam}
+      />
     </div>
   )
 }
