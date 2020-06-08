5e679773d7d68110f7432d0034e668ab85ef4e0b
JompAa <joonas.aaltonen@me.com>
2020-06-08 14:58:24 -0700

    ex 2.15 - 2.18

--- part2/phonebook/src/components/Display.js
+++ part2/phonebook/src/components/Display.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import Contact from './Contact';
 
-const Display = ({ persons, searchParam }) => {
+const Display = ({ persons, searchParam, deleteContact }) => {
 
   const searchOutput = persons.filter(person => person.name.toUpperCase().includes(searchParam.toUpperCase()))
 
   return (
-    <div>
+    <ul>
     {
       searchOutput.map((person) => {
-        return <p key={person.name}>{person.name} {person.number}</p>
+        return (
+          <Contact key={person.name} name={person.name} number={person.number} deleteContact={deleteContact} id={person.id}/>
+        )
       }) 
     }
-    </div>
+    </ul>
     
   )
 }
