c7d98d7812fdab163eb718b3627e96ac599f76a4
JompAa <joonas.aaltonen@me.com>
2020-06-19 10:22:36 -0700

    phonebook_backend

---
+++ part3/phonebook_backend/build/precache-manifest.38ff0ce80fd8c30b3c66de334594bdd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a3a810e9dc76c4618f090439c58aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c0ded825a44e88b1bb",
+    "url": "/static/js/main.013b3887.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
