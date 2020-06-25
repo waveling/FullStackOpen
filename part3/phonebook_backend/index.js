8bfed7e9dda62cd8e1d18c3b531a49399506618c
JompAa <joonas.aaltonen@me.com>
2020-06-25 15:01:45 -0700

    3.15-3.18

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,113 +1,143 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
-//Custom token to show request body if request method is POST
+//Custom morgan token to show request body if request method is POST
 morgan.token('content', function (req, res) {
   if (req.method === 'POST') {
     return JSON.stringify(req.body);
   }});
 
-app.use(cors());
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
-app.use(express.static('build'));
-
-//Hard coded list of contacts in the phonebook
-let persons = [
-      {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
+  
+  app.use(express.static('build'));
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
+  
+  //Hard coded list of contacts in the phonebook
+  let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ];
+  
+  //Route for displaying all contacts
+  app.get('/api/persons', (req, res) => {
+    Person.find({}).then(persons => {
+      res.json(persons)
+    })
+  });
+  
+  //Route for displaying total number of contacts and time the request was made.
+  app.get('/info', (req, res) => {
+    Person.find({}).then(persons => {
+      res.send(`<p>There are ${persons.length} contacts in the phonebook.</p><p>${new Date()}</p>`)
+    })
+    /* res.send(`<p>There are ${res.length} contacts in the phonebook.</p><p>${new Date()}</p>`); */
+  });
+  
+  //Route for displaying specific contact based on the id
+  app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
       }
-];
+    })
+    .catch(error => next(error))
+  })
 
-//Route for displaying all contacts
-app.get('/api/persons', (req, res) => {
-  res.send(persons);
-});
+  //Route for updating contact
+  app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
 
-//Route for displaying total number of contacts and time the request was made.
-app.get('/info', (req, res) => {
-  const count = persons.length;
-  res.send(`<p>There are ${count} contacts in the phonebook.</p><p>${new Date()}</p>`);
-});
+    const person = {
+      name: body.name,
+      number: body.number
+    }
 
-//Route for displaying specific contact based on the id in the URL
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-});
-
-//Route for deleting contact based on the id in the URL
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+      .then(updatedContact => {
+        res.json(updatedContact)
+      })
+      .catch(error => next(error))
+  })
   
-  res.status(204).end();
-});
-
-/* Generate ID for each added contact
-const generateId = () => {
-  const maxId = persons.length > 0
-  ? Math.max(...persons.map(person => person.id))
-  : 0;
-  return maxId + 1;
-} */
-
-//Route for adding contacts to the phonebook
-app.post('/api/persons', (req, res) => {
-  const body = req.body;
+  //Route for deleting contact based on the id
+  app.delete('/api/persons/:id', (req, res) => {
+    Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => console.log(error))
+  });
   
-  if (!body.name) {
-    return res.status(400).json({
-      error: 'Name is missing!'
-    });
-  } else if (!body.number) {
-    return res.status(400).json({
-      error: 'Number is missing!'
-    });
-  } else if (persons.some(person => person.name === body.name)) {
-    return res.status(400).json({
-      error: 'Name must be unique!'
-    });
-  };
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: Math.floor(Math.random() * 10000000)
+  //Route for adding contacts to the phonebook
+  app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    
+    if (!body.name) {
+      return res.status(400).json({
+        error: 'Name is missing!'
+      });
+    } else if (!body.number) {
+      return res.status(400).json({
+        error: 'Number is missing!'
+      });
+    } else if (persons.some(person => person.name === body.name)) {
+      return res.status(400).json({
+        error: 'Name must be unique!'
+      });
+    };
+    
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+      id: Math.floor(Math.random() * 10000000)
+    })
+    
+    person
+    .save()
+    .then(newPerson => res.json(newPerson))
+    .then(newPerson => newPerson.toJSON())
+  });
+  
+  //Error handler middleware
+  const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
   }
 
-  persons = persons.concat(person);
-
-  res.json(person);
-});
-
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.use(errorHandler);
+  
+  const PORT = process.env.PORT;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -6,30 +6,30 @@ const url = `mongodb+srv://fullstack:${password}@cluster0-1nf5r.mongodb.net/phon
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const contactSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
+const Person = mongoose.model('Person', personSchema);
 
-const contact = new Contact({
+const person = new Person({
   name: process.argv[3],
   number: process.argv[4],
 });
 
 //If user provided name & number with password, adds the contact to db; else prints existing contact to console.
 if (!process.argv[3]) {
-  Contact.find({}).then(result => {
+  Person.find({}).then(result => {
     console.log('Phonebook:')
-    result.forEach(contact => {
-      console.log(contact.name, contact.number);
+    result.forEach(person => {
+      console.log(person.name, person.number);
     });
     mongoose.connection.close();
   })
 } else {
-  contact.save().then(result => {
-    console.log(`Added ${contact.name} number ${contact.number} to phonebook!`);
+  person.save().then(result => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook!`);
     mongoose.connection.close();
   });
 }
