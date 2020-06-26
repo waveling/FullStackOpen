d6293b9f157b86e0fbc1ec3929dd106bc1c8a22e
JompAa <joonas.aaltonen@me.com>
2020-06-25 17:01:08 -0700

    cleanup

--- part2/phonebook/src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Contact = ({ name, number, deleteContact, id }) => {
-  return (
-    <li> { name } { number } <button onClick={deleteContact(id, name)}>Delete</button></li>
-  )
-}
-
-export default Contact;
