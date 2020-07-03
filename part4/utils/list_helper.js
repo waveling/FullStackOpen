91dde25402c8667be529d0de1c8a4bd8213c990a
JompAa <joonas.aaltonen@me.com>
2020-07-03 14:45:38 -0700

    4.6

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,92 @@
+const _ = require('lodash');
+
+const blogs = [ 
+  { 
+    _id: "5a422a851b54a676234d17f7", 
+    title: "React patterns", 
+    author: "Michael Chan", 
+    url: "https://reactpatterns.com/", 
+    likes: 7, 
+    __v: 0 
+  }, 
+  { 
+    _id: "5a422aa71b54a676234d17f8", 
+    title: "Go To Statement Considered Harmful", 
+    author: "Edsger W. Dijkstra", 
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", 
+    likes: 5, 
+    __v: 0 
+  },
+  { 
+    _id: "5a422b3a1b54a676234d17f9", 
+    title: "Canonical string reduction", 
+    author: "Edsger W. Dijkstra", 
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", 
+    likes: 12, 
+    __v: 0 
+  },
+  { 
+    _id: "5a422b891b54a676234d17fa", 
+    title: "First class tests", 
+    author: "Robert C. Martin", 
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", 
+    likes: 10, 
+    __v: 0 
+  },
+  { 
+    _id: "5a422ba71b54a676234d17fb", 
+    title: "TDD harms architecture", 
+    author: "Robert C. Martin", 
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", 
+    likes: 0, 
+    __v: 0 
+  },
+  { 
+    _id: "5a422bc61b54a676234d17fc", 
+    title: "Type wars", 
+    author: "Robert C. Martin", 
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", 
+    likes: 2, 
+    __v: 0 
+  }
+]
+
+//returns 1 regardless of the input
+const dummy = (blogs) => {
+  return 1;
+};
+
+//sums the likes of all the provided blogs and returns the total amount
+const totalLikes = (blogs) => {
+  return blogs.reduce((result, item) => result + item.likes, 0);
+};
+
+//searched for the blog with the most likes, and returns that blog's information
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((acc, cv) => {
+    return cv.likes > acc.likes ? cv : acc;
+  })
+};
+
+//searches for the author with most blogs. Returns the author and the amount of blogs.
+const mostBlogs = (blogs) => {
+  const groupedList = _.groupBy(blogs, function(blogs) {
+    return blogs.author; //returns an object with the authors as keys and arrays of objects as values
+  });
+
+  const reduced = _.reduce(groupedList, function(sum, n) {
+    
+    return sum.length > n.length ? sum : n;
+  }, {});
+
+  return { author: reduced[0].author, blogs: reduced.length };
+}
+
+console.log(mostBlogs(blogs));
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
+};
