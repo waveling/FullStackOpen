024e615ad3fa9629e93b45018f7a3fa22ccfd959
JompAa <joonas.aaltonen@me.com>
2020-07-16 13:39:49 -0700

    4.13 - 4.14

--- part4/controllers/blogList.js
+++ part4/controllers/blogList.js
@@ -1,22 +1,37 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+
+  response.json(blogs.map(blog => blog.toJSON()))
 });
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    });
+  try {
+    const newBlog = await blog.save()
+    response.json(newBlog)
+  } catch (error) {
+    next(error)
+  }  
+});
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end();
 });
 
 module.exports = blogsRouter;
