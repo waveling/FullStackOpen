9aaf06beeb95da02312cee8a1350edf1bd1a80fa
JompAa <joonas.aaltonen@me.com>
2020-08-20 15:15:33 -0700

    5.6-5.9

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,34 @@
-import React from 'react';
-const Blog = ({ blog }) => {
-  return (
-    <ul>
-      {blog.title}  {blog.author}
-    </ul>
-  )
+import React, { useState } from 'react';
+
+const Blog = ({ blog, handleLikes }) => {
+  const [details, setDetails] = useState(true);
+
+  const show = {display: details ? 'none' : ''}
+
+  const handleDetails = () => {
+    setDetails(!details)
+  }
+
+  const updateLikes = () => {
+    handleLikes({
+      ...blog,
+      likes: blog.likes + 1,
+    })
+  }
+    
+    return (
+      <div className='blogItem'>
+        <ul>
+          {blog.title} by {blog.author}
+        <button className='detailButton' onClick={handleDetails}>{details ? 'show' : 'hide'}</button>
+        <div style={show}>
+          <p>Likes: {blog.likes}<button onClick={updateLikes} className='detailButton'>like</button></p>
+          <p>Url: {blog.url}</p>
+          <p>User: {blog.user ? blog.user.name : 'N/A'}</p>
+        </div>
+        </ul>
+      </div>
+    )
 }
 
 
