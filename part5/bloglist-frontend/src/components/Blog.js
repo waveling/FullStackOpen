db3c28d7f4a7fdc065c4a8a2bd4ae256bdd2c783
JompAa <joonas.aaltonen@me.com>
2020-10-08 14:15:43 -0700

    5.14

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,59 +1,63 @@
 import React, { useState } from 'react'
 
 const Blog = ({ user, blog, handleLikes, handleDelete }) => {
-  const [details, setDetails] = useState(false)
+    const [details, setDetails] = useState(false)
 
-  const handleDetails = () => {
-    setDetails(!details)
-  }
-
-  const updateLikes = () => {
-    handleLikes({
-      ...blog,
-      likes: blog.likes + 1,
-    })
-  }
-
-  const removeBlog = () => {
-    if (window.confirm(`Are you sure you want to delete the blog ${blog.title}?`)) {
-      handleDelete(blog)
+    const handleDetails = () => {
+        setDetails(!details)
     }
-  }
 
-  const authorizeRemove = (identifiedUser) => {
-    if (blog.user && identifiedUser === blog.user.name) {
-      return true
+    const updateLikes = () => {
+        handleLikes({
+            ...blog,
+            likes: blog.likes + 1,
+        })
     }
-  }
 
-  return (
-    <div className='blogItem'>
-      <ul>
-        <p className='title' >
-          {blog.title}
-          <button className='detailButton' onClick={handleDetails}>{details ? 'hide' : 'show'}</button>
-        </p>
-        <p className='author'>
-          {blog.author}
-        </p>
-        {
-          details && (
-            <div>
-              <p>Likes: {blog.likes}</p>
-              <p>Url: {blog.url}</p>
-              <p>User: {blog.user ? blog.user.name : 'N/A'}</p>
-              <button onClick={updateLikes} className='like'>like</button>
-              {
-                authorizeRemove(user.name) &&
-                <button onClick={removeBlog} className='remove'>remove</button>
-              }
-            </div>
-          )
+    const removeBlog = () => {
+        if (
+            window.confirm(
+                `Are you sure you want to delete the blog ${blog.title}?`
+            )
+        ) {
+            handleDelete(blog)
         }
-      </ul>
-    </div>
-  )
-}
+    }
 
+    const authorizeRemove = (identifiedUser) => {
+        if (blog.user && identifiedUser === blog.user.name) {
+            return true
+        }
+    }
+
+    return (
+        <div className="blogItem">
+            <ul>
+                <p className="title">
+                    {blog.title}
+                    <button className="detailButton" onClick={handleDetails}>
+                        {details ? 'hide' : 'show'}
+                    </button>
+                </p>
+                <p className="author">{blog.author}</p>
+                {details && (
+                    <div>
+                        <p className="likes">Likes: {blog.likes}</p>
+                        <p className="url">Url: {blog.url}</p>
+                        <p>User: {blog.user ? blog.user.name : 'N/A'}</p>
+                        <button onClick={updateLikes} className="like">
+                            like
+                        </button>
+                        {authorizeRemove(user.name) && (
+                            <button onClick={removeBlog} className="remove">
+                                remove
+                            </button>
+                        )}
+                    </div>
+                )}
+            </ul>
+        </div>
+    )
+}
 
 export default Blog
--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,43 +1,53 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  let component
-  const user = {
-    id: 'abcdefghijklmn',
-    name: 'Itsa Test',
-    username: 'testerbot',
-  }
+    let component
+    const user = {
+        id: 'abcdefghijklmn',
+        name: 'Itsa Test',
+        username: 'testerbot',
+    }
 
-  const blog = {
-    likes: 10,
-    author: 'Jari Tervo',
-    url: 'https://randomurl.com',
-    title: 'Not A Real Title',
-    user: {
-      id: 'abcdefghijklmn',
-      name: 'Itsa Test',
-      username: 'testerbot',
-    },
-    id: 'iuhsdiufs8fsdhfs8fsjdhf9',
-  }
+    const blog = {
+        likes: 10,
+        author: 'Jari Tervo',
+        url: 'https://randomurl.com',
+        title: 'Not A Real Title',
+        user: {
+            id: 'abcdefghijklmn',
+            name: 'Itsa Test',
+            username: 'testerbot',
+        },
+        id: 'iuhsdiufs8fsdhfs8fsjdhf9',
+    }
 
-  beforeEach(() => {
-    component = render(<Blog blog={blog} user={user} />)
-  })
+    beforeEach(() => {
+        component = render(<Blog blog={blog} user={user} />)
+    })
 
-  test('render title and author', () => {
-    expect(component.container.querySelector('.title')).toHaveTextContent(
-      blog.title
-    )
-    expect(component.container.querySelector('.author')).toHaveTextContent(
-      blog.author
-    )
-    expect(component.queryByText(blog.url)).not.toBeInTheDocument()
-    expect(component.queryByText('like')).not.toBeInTheDocument()
-  })
+    test('render title and author', () => {
+        expect(component.container.querySelector('.title')).toHaveTextContent(
+            blog.title
+        )
+        expect(component.container.querySelector('.author')).toHaveTextContent(
+            blog.author
+        )
+        expect(component.queryByText(blog.url)).not.toBeInTheDocument()
+        expect(component.queryByText('like')).not.toBeInTheDocument()
+    })
 
-  test('additional information can be toggled', () => {})
+    test('additional information can be toggled', () => {
+        const button = component.getByText('show')
+        fireEvent.click(button)
+
+        expect(component.container.querySelector('.url')).toHaveTextContent(
+            blog.url
+        )
+        expect(component.container.querySelector('.likes')).toHaveTextContent(
+            blog.likes
+        )
+    })
 })
