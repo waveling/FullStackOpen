54aee79bb5cbaa43fd7bcf515eea7f86de1c6ea6
JompAa <joonas.aaltonen@me.com>
2020-10-14 13:49:35 -0700

    5.18

--- part5/bloglist-frontend/cypress/integration/note_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/note_app.spec.js
@@ -1,10 +1,37 @@
 describe('Bloglist app', function () {
     beforeEach(function () {
-        cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', {
+            username: 'testerbot',
+            name: 'Itsa Test',
+            password: 'salasana',
+        })
         cy.visit('http://localhost:3000')
     })
 
-    it('front page can be opened', function () {
-        cy.contains('login').click()
+    it('Login form is shown', function () {
+        cy.contains('login')
+    })
+
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
+            cy.get('#username').type('testerbot')
+            cy.get('#password').type('salasana')
+            cy.get('#login-button').click()
+
+            cy.contains('Itsa Test is logged in')
+        })
+
+        it.only('fails with wrong credentials', function () {
+            cy.contains('login').click()
+            cy.get('#username').type('testerbot')
+            cy.get('#password').type('wrongpassword')
+            cy.get('#login-button').click()
+
+            cy.get('.error')
+                .should('contain', 'Wrong credentials')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+                .and('have.css', 'border-style', 'solid')
+        })
     })
 })
--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,15 +1,11 @@
 import React from 'react'
 
 const Notification = ({ message }) => {
-  if (message === null) {
-    return null
-  }
+    if (message === null) {
+        return null
+    }
 
-  return (
-    <div className={message.type}>
-      { message.text }
-    </div>
-  )
+    return <div className={message.type}>{message.text}</div>
 }
 
 export default Notification
