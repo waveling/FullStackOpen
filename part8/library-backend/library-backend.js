216c3fcc482309792f21c1478ceebee34a6cb94d
Joonas Aaltonen <joonas.aaltonen@me.com>
2021-03-04 13:56:44 -0800

    8.5

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks(author: String): [Book!]!
+        allBooks(author: String, genre: String): [Book!]!
         allAuthors: [Author]
     }
 `
@@ -112,11 +112,19 @@ const resolvers = {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
 		allBooks: (root, args) => {
-			if (!args.author) {
+			if (!args.author && !args.genre) {
 				return books
-			} else {
+			} else if (args.author && !args.genre){
 				return books.filter(b => {
 					return args.author === b.author
+				})
+			} else if (!args.author && args.genre) {
+				return books.filter(b => {
+					return b.genres.includes(args.genre)
+				})
+			} else {
+				return books.filter(b => {
+					return args.author === b.author && b.genres.includes(args.genre)
 				})
 			}
 		},
